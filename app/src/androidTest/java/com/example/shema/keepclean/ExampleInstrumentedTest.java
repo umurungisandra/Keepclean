890fec4aac37b80973cc9d40c36de4d308a4ecaa
oynomak <oynomak@gmail.com>
2017-08-20 07:50:34 +0200

    kamonyo: Changed the .gitignore to prevent the project to commint the .idea folder. Also tried to rename the packages

--- app/src/androidTest/java/com/example/shema/keepclean/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/shema/keepclean/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.shema.keepclean", appContext.getPackageName());
+        assertEquals("com.example.shema.rw.mint.rw.mint.rw.mint.keepclean", appContext.getPackageName());
     }
 }
