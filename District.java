4c0589745a6b3d0dc65a6282d8879e5e1afe2542
umurungisandra <umuru20@gmail.com>
2017-08-22 15:17:08 +0200

    district class

---
+++ District.java
@@ -0,0 +1,17 @@
+package rw.mint.keepclean;
+
+import android.os.Parcelable;
+
+/**
+ * Created by Shema on 8/22/2017.
+ */
+
+public class District {
+    Integer cleanerId;
+    String name;
+    String phoneNum;
+    String email;
+    District district;
+
+
+}
