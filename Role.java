08047a80fabe537b4d854625ad18844c303a7128
umurungisandra <umuru20@gmail.com>
2017-08-22 19:34:35 +0200

    add constructor and getter&setter

--- Role.java
+++ Role.java
@@ -7,4 +7,25 @@ package rw.mint.keepclean;
 public class Role {
     String role;
     String description;
+
+    public Role(String role, String description) {
+        this.role = role;
+        this.description = description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
